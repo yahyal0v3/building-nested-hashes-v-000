74fdfd4c08fdbca0a50eba1f8b3c55a4a73d7c7f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-06 02:19:42 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attitude: "worried"}
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -22,6 +22,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
